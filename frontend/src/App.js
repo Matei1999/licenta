0a9f608b22181336fb0daad878cddeb2645b09bd
Matei Ionescu <matei.ionescu18@gmail.com>
2026-01-11 13:18:51 +0200

    Am adaugat Hotjar pentru heatmap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,28 @@ function App() {
     };
   }, []);
 
+  // Hotjar: inject script when ID is provided (typically in production)
+  useEffect(() => {
+    const hotjarId = process.env.REACT_APP_HOTJAR_ID;
+    const hotjarVersion = process.env.REACT_APP_HOTJAR_VERSION || 6;
+    if (!hotjarId) return; // no ID, skip
+    if (window.hj && window._hjSettings?.hjid === Number(hotjarId)) return; // already loaded
+
+    /* eslint-disable */
+    (function(h, o, t, j, a, r) {
+      h.hj = h.hj || function() {
+        (h.hj.q = h.hj.q || []).push(arguments);
+      };
+      h._hjSettings = { hjid: Number(hotjarId), hjsv: hotjarVersion };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    /* eslint-enable */
+  }, []);
+
   return (
     <AuthProvider>
       <Router>
